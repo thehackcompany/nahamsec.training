f35e7ebffeaee9f91d0da424ae74245c274b14cc
igibek <koishybayev.igibek@gmail.com>
2021-03-23 19:00:57 -0400

    localized list.php

--- app/training/templates/list.php
+++ app/training/templates/list.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Nahamsec Training Udemy Course</title>
+    <title>TheHack | Hacking-101</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,12 +12,15 @@
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
 
-            <h1 class="text-center">Nahamsec Training</h1>
-            <h5 class="text-center">These labs have been developed for the udemy course organized by <a href="https://twitter.com/nahamsec">Nahamsec</a></h5>
+            <h1 class="text-center">TheHack Lab: Hacking 101</h1>
+            <!-- Credits: These labs originally were developed for Udemy course organized by  Nahamsec (twitter: @nahamsec) 
+                I localized and updated the labs to my needs
+            -->
+            <h5 class="text-center">Бұл лабораториялық тапсырмалар <a href="https://thehack.kz/course/hacking-101">"Hacking-101"</a> курсына арналып жасалған. Толығырақ <a href="https://thehack.kz">TheHACK</a></h5>
 
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Challenges</h3>
+                    <h3 class="panel-title">лабораториялық жұмыстар</h3>
                 </div>
                 <div class="panel-body">
                     <ul>
